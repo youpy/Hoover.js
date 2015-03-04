b75eb0b43264cf94d659a565409bf1daf32a5add
youpy <youpy@buycheapviagraonlinenow.com>
2015-03-04 18:24:58 +0900

    fix view

--- example/app.js
+++ example/app.js
@@ -4,11 +4,20 @@ var neu = neume(new AudioContext());
 var _ = neume._;
 var mouse = { y: 290 };
 
+function setMouseY(y) {
+  mouse.y = _.midicps(_.linlin(y, 0, 1, 72, 48) | 0);
+}
+
 // https://github.com/mohayonao/neume.js/wiki/snippet-mouse
 window.addEventListener('mousemove', function(e) {
   var y = e.pageY / window.innerHeight;
-  mouse.y = _.midicps(_.linlin(y, 0, 1, 72, 48)|0);
+  setMouseY(y);
 });
 
-neu.Synth(Hoover, mouse, { key: "y", curve: "exp", lag: 0.200 }).start();
+window.addEventListener('touchmove', function(e) {
+  var y = e.touches[0].pageY / window.innerHeight;
+  setMouseY(y);
+});
+
+neu.Synth(Hoover, mouse, { key: 'y', curve: 'exp', lag: 0.200 }).start();
 //neu.Synth(Hoover).start();
