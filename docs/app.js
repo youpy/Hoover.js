0785bb8bb2cbb9f1105de684c3d56c8eb18c5f1b
youpy <youpy@buycheapviagraonlinenow.com>
2017-08-07 10:53:36 +0900

    add docs/

---
+++ docs/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var hoover = new Hoover.Hoover(new AudioContext());
+
+hoover.frequency.value = 293.6647679174076;
+hoover.connect(hoover.context.destination);
+hoover.start();
+
+function bend(y) {
+  hoover.frequency.exponentialRampToValueAtTime(
+    130 + (400 * (1.0 - (y / window.innerHeight))),
+    hoover.context.currentTime + 0.2
+  );
+}
+
+window.addEventListener('mousemove', function(e) {
+  bend(e.pageY);
+});
+
+window.addEventListener('touchmove', function(e) {
+  bend(e.touches[0].pageY);
+});
