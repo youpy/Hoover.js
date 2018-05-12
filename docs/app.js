2e2d2d3082fba57cee81f0d8f467d3b1d950a231
youpy <youpy@buycheapviagraonlinenow.com>
2018-05-12 23:20:45 +0900

    fix docs

--- docs/app.js
+++ docs/app.js
@@ -2,27 +2,27 @@
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-var hoover = new Hoover.Hoover(new AudioContext());
-
-hoover.frequency.value = 293.6647679174076;
-hoover.connect(hoover.context.destination);
-
 var button = document.querySelector('button');
 button.addEventListener('click', function() {
+  var hoover = new Hoover.Hoover(new AudioContext());
+
+  hoover.frequency.value = 293.6647679174076;
+  hoover.connect(hoover.context.destination);
+
   hoover.start();
+
+  function bend(y) {
+    hoover.frequency.exponentialRampToValueAtTime(
+      130 + (400 * (1.0 - (y / window.innerHeight))),
+      hoover.context.currentTime + 0.2
+    );
+  }
+
+  window.addEventListener('mousemove', function(e) {
+    bend(e.pageY);
+  });
+
+  window.addEventListener('touchmove', function(e) {
+    bend(e.touches[0].pageY);
+  });
 })
-
-function bend(y) {
-  hoover.frequency.exponentialRampToValueAtTime(
-    130 + (400 * (1.0 - (y / window.innerHeight))),
-    hoover.context.currentTime + 0.2
-  );
-}
-
-window.addEventListener('mousemove', function(e) {
-  bend(e.pageY);
-});
-
-window.addEventListener('touchmove', function(e) {
-  bend(e.touches[0].pageY);
-});
